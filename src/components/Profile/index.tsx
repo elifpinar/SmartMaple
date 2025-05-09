a87b00b236ec5f7679bf16a6c1cf121096390ef1
elifpinar <elifpinarxx@gmail.com>
2025-05-09 19:44:20 +0300

    1. role / scroll bar

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -3,16 +3,27 @@ import AuthSession from "../../utils/session";
 import "../profileCalendar.scss";
 
 type ProfileCardProps = {
-    profile: UserInstance;
+  profile?: UserInstance;
 };
 
 const ProfileCard = ({ profile }: ProfileCardProps) => {
+  console.log("profile:", JSON.stringify(profile, null, 2));
+
+  // Eğer profile varsa, önce oradan al; yoksa AuthSession'dan
+  const roles = profile?.role
+    ? profile.role
+    : AuthSession.getRoles();
+
+  const roleName = Array.isArray(roles)
+    ? roles.map((role) => role.name).join(", ")
+    : roles?.name;
+
   return (
     <div className="profile-section">
       <div className="profile-info">
-        <h2>Welcome, {profile?.name}</h2>
+        <h2>Welcome, {profile?.name ?? AuthSession.getName()}</h2>
         <p>{profile?.email ?? AuthSession.getEmail()}</p>
-        <p>{/*profile?.role ??*/ AuthSession.getRoles()}</p>
+        <p>{roleName}</p>
       </div>
     </div>
   );
